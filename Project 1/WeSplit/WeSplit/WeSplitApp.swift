dd6954c6c2ac028433b5de3ef72d65e2c01d8156
Willie Pai <PaisWillie@gmail.com>
2023-11-27 16:33:10 -0500

    Initial commit

---
+++ Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Willie Pai on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
