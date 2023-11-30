ca64eff932f901ac87c152d71576036944f497a6
Willie Pai <PaisWillie@gmail.com>
2023-11-30 16:59:32 -0500

    feat: day 31

---
+++ Project 5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Willie Pai on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
