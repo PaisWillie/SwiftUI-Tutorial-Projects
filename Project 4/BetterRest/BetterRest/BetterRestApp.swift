610b77ecc3df8a30af1ef007c7b0538900ddb1b3
Willie Pai <PaisWillie@gmail.com>
2023-11-30 11:30:11 -0500

    feat: day 28

---
+++ Project 4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Willie Pai on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
